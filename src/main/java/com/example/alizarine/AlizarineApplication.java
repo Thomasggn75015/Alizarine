7000d3de38e4f8226cd5e45c174af5f5df9f7592
Thomas <thomas.gagnier0@gmail.com>
2022-05-18 14:36:00 +0200

    first commit

---
+++ src/main/java/com/example/alizarine/AlizarineApplication.java
@@ -0,0 +1,13 @@
+package com.example.alizarine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlizarineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlizarineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/alizarine/AlizarineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alizarine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlizarineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
